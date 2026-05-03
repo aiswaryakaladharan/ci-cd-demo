b150e23fd43561118f162b0710d55ba3e4ae851b
Aiswarya Kaladharan <70939010+aiswaryakaladharan@users.noreply.github.com>
2026-05-03 12:44:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello CI/CD Pipeline");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from AKS CI/CD Pipeline!');
+});
+
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
